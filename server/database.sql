a8f10c300ea18babc869f4bfb0cf3aff47b17a15
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2022-01-24 14:02:45 +0700

    Fix main routes

--- server/database.sql
+++ server/database.sql
@@ -14,26 +14,26 @@ CREATE TABLE player(
 
 CREATE TABLE participates_on(
   p_id INTEGER,
-  t_id INTEGER,
+  tour_id INTEGER,
   p_point INTEGER DEFAULT 0,
-  PRIMARY KEY (p_id, t_id),
+  PRIMARY KEY (p_id, tour_id),
   CONSTRAINT fk_player
     FOREIGN KEY(p_id) 
 	    REFERENCES player(p_id),
   CONSTRAINT fk_tournament
-    FOREIGN KEY(t_id) 
+    FOREIGN KEY(tour_id) 
 	    REFERENCES tournament(tour_id)
 );
 
 CREATE TABLE match(
   match_id SERIAL PRIMARY KEY,
-  t_id INTEGER NOT NULL,
+  tour_id INTEGER NOT NULL,
   p1_id INTEGER NOT NULL,
   p2_id INTEGER NOT NULL,
   score_p1 INTEGER,
   score_p2 INTEGER,
   CONSTRAINT fk_tournament
-    FOREIGN KEY(t_id) 
+    FOREIGN KEY(tour_id) 
 	    REFERENCES tournament(tour_id),
   CONSTRAINT fk_player1
     FOREIGN KEY (p1_id) 
@@ -43,11 +43,11 @@ CREATE TABLE match(
       REFERENCES player(p_id)
 );
 
--- INSERT DATAS
+-- INSERT DATAS EXAMPLE
 INSERT INTO tournament(tour_name) VALUES('eFCS Super League');
 INSERT INTO player(p_name) VALUES('Tanlee'), ('Gytzero'), ('Prawnpaste');
-INSERT INTO participates_on(p_id, t_id) VALUES(1, 1), (2, 1), (3, 1);
-INSERT INTO match(t_id, p1_id, p2_id) VALUES(1, 1, 2), (1, 1, 3), (1, 2, 3);
+INSERT INTO participates_on(p_id, tour_id) VALUES(1, 1), (2, 1), (3, 1);
+INSERT INTO match(tour_id, p1_id, p2_id) VALUES(1, 1, 2), (1, 1, 3), (1, 2, 3);
 
 
 -- CHEATSHEET:
--- server/index.js
+++ server/index.js
@@ -3,19 +3,81 @@ const app = express();
 const cors = require("cors")
 const pool = require("./db")
 
-// middleware
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-// routes
-// POST a match
+// ROUTES
+// Get all players
+app.get("/player", async (req, res) => {
+  try {
+    const allPlayers = await pool.query("SELECT * FROM player;");
+    res.json(allPlayers.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// Create a player (user)
+app.post("/player", async (req, res) => {
+  try {
+    const { p_name } = req.body;
+    const newPlayer = await pool.query(
+      "INSERT INTO player(p_name) VALUES($1) RETURNING *;",
+      [p_name]
+    )
+    res.json(newPlayer.rows[0]);
+
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// Get all tournaments
+app.get("/tournament", async (req, res) => {
+  try {
+    const allTournaments = await pool.query("SELECT * FROM tournament;");
+    res.json(allTournaments.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// Create a tournament
+app.post("/tournament", async (req, res) => {
+  try {
+    const { tour_name } = req.body;
+    const newTournament = await pool.query(
+      "INSERT INTO tournament(tour_name) VALUES($1) RETURNING *;",
+      [tour_name]
+    )
+    res.json(newTournament.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// Enter a tournament
+app.post("/tournament-enter", async (req, res) => {
+  try {
+    const { p_id, tour_id } = req.body;
+    const newParticipation = await pool.query(
+      "INSERT INTO participates_on(p_id, tour_id) VALUES($1, $2) RETURNING *;",
+      [p_id, tour_id]
+    )
+    res.json(newParticipation.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// Create a match
 app.post("/match", async (req, res) => {
   try {
-    console.log(req.body);
-    const { player_1, player_2 } = req.body;
+    const { tour_id, p1_id, p2_id } = req.body;
     const newMatch = await pool.query(
-      "INSERT INTO match(player_1, player_2) VALUES($1, $2) RETURNING *", 
-      [player_1, player_2]
+      "INSERT INTO match(tour_id, p1_id, p2_id) VALUES($1, $2, $3) RETURNING *;", 
+      [tour_id, p1_id, p2_id]
     );
     res.json(newMatch.rows[0]);
   } catch (error) {
@@ -23,22 +85,22 @@ app.post("/match", async (req, res) => {
   }
 })
 
-// GET all matches
+// Get all matches
 app.get("/match", async (req, res) => {
   try {
-    const allMatches = await pool.query("SELECT * FROM match");
+    const allMatches = await pool.query("SELECT * FROM match;");
     res.json(allMatches.rows);
   } catch (error) {
     console.error(error.message);
   }
 })
 
-// GET match by id
+// Get match by id
 app.get("/match/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const match = await pool.query(
-      "SELECT * FROM match WHERE match_id = $1", 
+      "SELECT * FROM match WHERE match_id = $1;", 
       [id]
     );
     res.json(match.rows);
@@ -47,27 +109,27 @@ app.get("/match/:id", async (req, res) => {
   }
 })
 
-// PUT a match
+// Update match score
 app.put("/match/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { player_1, player_2 } = req.body;
+    const { score_p1, score_p2 } = req.body;
     const updateMatch = await pool.query(
-      "UPDATE match SET player_1 = $1, player_2 = $2 WHERE match_id = $3", 
-      [player_1, player_2, id]
+      "UPDATE match SET score_p1 = $1, score_p2 = $2 WHERE match_id = $3;", 
+      [score_p1, score_p2, id]
     );
-    res.json("Match updated");
+    res.json("Match score updated");
   } catch (error) {
     console.error(error.message);
   }
 })
 
-// DELETE a match
+// Delete a match
 app.delete("/match/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const deleteMatch = await pool.query(
-      "DELETE FROM match WHERE match_id = $1",
+      "DELETE FROM match WHERE match_id = $1;",
       [id]
     );
     res.json("Match deleted")
@@ -76,6 +138,7 @@ app.delete("/match/:id", async (req, res) => {
   }
 })
 
+// Start server
 app.listen(5000, () => {
-  console.log("Server started on port 5000")
+  console.log("Server started on port 5000.")
 })
